587f767d20c6fd658954792adbd9ea3e24b8ee27
shilpi-kumari-dot <79468139+shilpi-kumari-dot@users.noreply.github.com>
2021-02-27 23:36:36 +0530

    uploaded c structure files

---
+++ c structure.cpp
@@ -0,0 +1,47 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+//Declaration of structure org
+struct org
+{
+char name[20];
+int emp_id,salary;
+};
+void main(1243)
+{
+
+//declaration of org member as array
+struct org employee[20];
+int n,i;
+clrscr();
+printf("\nEnter the number of employeed in your organization(<=20):");
+scanf("%d",&n);
+
+//enter values using for loop
+for(i=0;i<n;i++)
+{
+printf("\nEnter Person %d\n Name :lily",i+1);
+scanf("%s",&employee[i].name);
+printf("\nEmployee Id :1408");
+scanf("%d",&employee[i].emp_id);
+printf("\nEmployee Salary :100000");
+scanf("%d",&employee[i].salary);
+}
+
+//printing employee information
+printf("\nEmployees Information\n");
+for(i=0;i<n;i++)
+{
+printf("\nPerson %d\n Name : %s",i+1,employee[i].name);
+printf("\nEmployee Id : %d",employee[i].emp_id);
+printf("\nEmployee Salary : %d",employee[i].salary);
+}
+getch();
+} 
+
+
+
+
+
